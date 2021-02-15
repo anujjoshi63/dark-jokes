7a4a709461d313716eb458c4db4ad68ad1419d61
Anuj Joshi <anujjoshi63@gmail.com>
2021-02-15 12:05:38 +0530

    Fix: Dark Mode only

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "./water.css";
 import "./App.css";
 function App() {
 	const [setup, setSetup] = useState("Click button for a joke!");
 	const [punch, setPunch] = useState("");
+	const [open, setOpen] = useState(true);
 	const fetchJoke = () => {
+		setOpen(false);
 		fetch("https://sv443.net/jokeapi/v2/joke/Dark?type=twopart")
-			.then((response) => response.json())
-			.then((data) => {
+			.then(response => response.json())
+			.then(data => {
 				setSetup(data.setup);
 				setPunch(data.delivery);
+				setOpen(true);
 			});
 	};
+	useEffect(() => {
+		document.title = "Dark Jokes!";
+	}, []);
 	return (
-		<div className="main" style={{ flex: 1 }}>
-			<link rel="stylesheet" href="https://unpkg.com/wingcss" />
-			<h3>{setup}</h3>
-			<h3>{punch}</h3>
-			<button
-				className="
-			center"
-				onClick={() => {
-					fetchJoke();
-				}}
-			>
-				Bring me a joke
-			</button>
-		</div>
+		<>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<title>Dark Jokes!</title>
+			<div className="main">
+				<div className="joke">{setup}</div>
+				<div className="joke">{punch}</div>
+				<div className="joke">
+					<button
+						className="center"
+						onClick={() => {
+							fetchJoke();
+						}}
+						disabled={!open}
+					>
+						Bring me a joke
+					</button>
+				</div>
+			</div>
+		</>
 	);
 }
 
